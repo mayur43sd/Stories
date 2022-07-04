495819e81a74bacbbcdbd33515d456c47fb78a5d
mayur43sd <mayursher>
2022-07-04 19:30:38 +0530

    rearranging for heroku deploy

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const multer = require('multer')
 const path = require('path')
 const PORT = process.env.PORT || '5000'
 
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    });
+   }
+
+
 dotenv.config()
 app.use(express.json())
 app.use('/images',express.static(path.join(__dirname,'/images')))
